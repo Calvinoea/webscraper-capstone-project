1e8915526c3f03f78d39c064066936268a32ed6a
calvinoea <calvinebunamu@gmail.com>
2020-05-18 16:07:29 +0100

    Rubocop checks made

--- bin/main.rb
+++ bin/main.rb
@@ -1,24 +1,21 @@
 #!/usr/bin/env ruby
-# frozen_string_literal:true
-
 require_relative '../lib/books_scraper'
 
 def scrape_books
-     books_scraper = Books.new
+  books_scraper = Books.new
 
-     puts "Input a name for your file with the Books Scraper."
+  puts 'Input a name for your file with the Books Scraper.'
 
-     name = gets.chomp
+  name = gets.chomp
 
-     json_array = books_scraper.get_top_books
+  json_array = books_scraper.top_books
 
-     if json_array
-      File.open("#{name}.txt",'w+') do |file|
-        json_array.each {|book| file.puts(book)}
-     end
-       puts "Your file, #{name}.txt has been made"
-    end
-   end
+  return unless json_array
 
-   scrape_books
+  File.open("#{name}.txt", 'w+') do |file|
+    json_array.each { |book| file.puts(book) }
+  end
+  puts "Your file, #{name}.txt has been made"
+end
 
+scrape_books
--- lib/books_scraper.rb
+++ lib/books_scraper.rb
@@ -1,49 +1,45 @@
-# frozen_string_literal:true
-
 require 'nokogiri'
 require 'open-uri'
 require 'json'
 
 class Books # :nodoc:
-  URL = 'https://www.waterstones.com/campaign/books-of-the-month'
+  URL = 'https://www.waterstones.com/campaign/books-of-the-month'.freeze
 
-      def initialize
-        @list_of_books = []
-        @json_array = []
-      end
+  def initialize
+    @list_of_books = []
+    @json_array = []
+  end
 
-      def get_top_books
-        hash = extract_books
-        if hash
-          hash.each {|h| @json_array << h.to_json}
-          @json_array
-        end
-      end
+  def top_books
+    hash = extract_books
+    return unless hash
 
-      private
+    hash.each { |h| @json_array << h.to_json }
+    @json_array
+  end
 
-      def make_html
-        begin
-          doc = Nokogiri::HTML(URI.parse(URL).open)
-          doc
-        rescue => error
-          puts "#{error.class} #{error.message}, Issue with connection"
-        end
-      end
+  private
 
-      def extract_books
-        if make_html
-          books = make_html.css('div.info-wrap')
-          books.each do |book|
-            hash = {
-            title: book.css('a.title').text,
-            author:book.css('a.text-gold').text,
-            old_price:book.css('span.price-rrp').text,
-            new_price:book.css('span.price').text
-            }
-            @list_of_books << hash
-          end
-          @list_of_books
-        end
-      end
+  def make_html
+    doc = Nokogiri::HTML(URI.parse(URL).open)
+    doc
+  rescue StandardError => e
+    puts "#{e.class} #{e.message}, Issue with connection"
+  end
+
+  def extract_books
+    return unless make_html
+
+    books = make_html.css('div.info-wrap')
+    books.each do |book|
+      hash = {
+        title: book.css('a.title').text,
+        author: book.css('a.text-gold').text,
+        old_price: book.css('span.price-rrp').text,
+        new_price: book.css('span.price').text
+      }
+      @list_of_books << hash
     end
+    @list_of_books
+  end
+end
